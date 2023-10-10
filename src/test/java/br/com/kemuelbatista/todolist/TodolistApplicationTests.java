6ef05de60a0efdb9de1c2a6fb49a83d012764061
Kemuel Batista <kemuellima20@gmail.com>
2023-10-10 16:51:25 -0300

    chore:initial commit

---
+++ src/test/java/br/com/kemuelbatista/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kemuelbatista.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
