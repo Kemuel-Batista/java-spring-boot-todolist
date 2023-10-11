dc6b008983f3b7330c1f17d6967774de92aa8dab
Kemuel Batista <kemuellima20@gmail.com>
2023-10-11 14:41:38 -0300

    heat: aula 03 finalizada

--- src/main/java/br/com/kemuelbatista/todolist/task/TaskModel.java
+++ src/main/java/br/com/kemuelbatista/todolist/task/TaskModel.java
@@ -1,4 +1,4 @@
-package br.com.kemuelbatista.todolist.user;
+package br.com.kemuelbatista.todolist.task;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -11,18 +11,24 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-@Data // getters and setters from a model
-@Entity(name = "tb_users")
-public class UserModel {
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
   @Id
   @GeneratedValue(generator = "UUID")
   private UUID id;
 
-  @Column(unique = true)
-  private String username;
-  private String name;
-  private String password;
+  private String description;
+
+  @Column(length = 50)
+  private String title;
+  private LocalDateTime startAt;
+  private LocalDateTime endAt;
+  private String priority;
+  private UUID idUser;
 
   @CreationTimestamp
   private LocalDateTime createdAt;
+
 }
