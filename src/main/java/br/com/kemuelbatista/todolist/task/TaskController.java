00bb52a8a7bd83cef476f73f21eb754d70c98c75
Kemuel Batista <kemuellima20@gmail.com>
2023-10-15 14:14:45 -0300

    heat: ajustes de exception handler

--- src/main/java/br/com/kemuelbatista/todolist/task/TaskController.java
+++ src/main/java/br/com/kemuelbatista/todolist/task/TaskController.java
@@ -30,14 +30,16 @@ public class TaskController {
     taskModel.setIdUser((UUID) idUser);
 
     var currentDate = LocalDateTime.now();
+    // 10/11/2023 - Current
+    // 10/10/2023 - startAt
     if (currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-          .body("A data de início / data de término deve ser maior do que a data atual!");
+          .body("A data de início / data de término deve ser maior do que a data atual");
     }
 
     if (taskModel.getStartAt().isAfter(taskModel.getEndAt())) {
       return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-          .body("A data de início deve ser menor do que a data de término!");
+          .body("A data de início deve ser menor que a data de término");
     }
 
     var task = this.taskRepository.save(taskModel);
@@ -47,18 +49,26 @@ public class TaskController {
   @GetMapping("/")
   public List<TaskModel> list(HttpServletRequest request) {
     var idUser = request.getAttribute("idUser");
-
     var tasks = this.taskRepository.findByIdUser((UUID) idUser);
-
     return tasks;
   }
 
   @PutMapping("/{id}")
-  public TaskModel update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
+  public ResponseEntity update(@RequestBody TaskModel taskModel, HttpServletRequest request, @PathVariable UUID id) {
     var task = this.taskRepository.findById(id).orElse(null);
 
-    Utils.copyNonNullProperties(taskModel, task);
+    if (task == null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Tarefa não encontrada");
+    }
 
-    return this.taskRepository.save(task);
+    var idUser = request.getAttribute("idUser");
+
+    if (!task.getIdUser().equals(idUser)) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário não tem permissão para alterar essa tarefa");
+    }
+
+    Utils.copyNonNullProperties(taskModel, task);
+    var taskUpdated = this.taskRepository.save(task);
+    return ResponseEntity.ok().body(taskUpdated);
   }
 }
--- src/main/java/br/com/kemuelbatista/todolist/task/TaskModel.java
+++ src/main/java/br/com/kemuelbatista/todolist/task/TaskModel.java
@@ -18,7 +18,6 @@ public class TaskModel {
   @Id
   @GeneratedValue(generator = "UUID")
   private UUID id;
-
   private String description;
 
   @Column(length = 50)
@@ -26,9 +25,17 @@ public class TaskModel {
   private LocalDateTime startAt;
   private LocalDateTime endAt;
   private String priority;
+
   private UUID idUser;
 
   @CreationTimestamp
   private LocalDateTime createdAt;
 
+  public void setTitle(String title) throws Exception {
+    if (title.length() > 50) {
+      throw new Exception("O campo title deve conter no máximo 50 caracteres");
+    }
+
+    this.title = title;
+  }
 }
